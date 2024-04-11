1feacdc1c255c902ce79e0e8ca60d03c0eedfe98
muro-yuta24-fixer <muro.yuta24@fixer.co.jp>
2024-04-11 09:59:29 +0900

    fix(nvim): theme font style

--- config/nvim/lua/plugins/onedarkpro.lua
+++ config/nvim/lua/plugins/onedarkpro.lua
@@ -2,7 +2,13 @@ return {
   "olimorris/onedarkpro.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    styles = {
+      comments = "italic",
+      types = "bold",
+      functions = "italic,bold",
+    },
+  },
   config = function(_, opts)
     require("onedarkpro").setup(opts)
     vim.cmd("colorscheme onedark")

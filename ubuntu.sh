56dd6bf5e4731b8d82e1066256280edbeb2c48cc
muro-yuta24-fixer <muro.yuta24@fixer.co.jp>
2024-05-01 11:12:54 +0900

    ✨ Ubuntu用のインストールスクリプトを追加

---
+++ ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+sudo sed -i "s|%sudo\tALL=(ALL:ALL) ALL|%sudo\tALL=(ALL:ALL) NOPASSWD:ALL|g" /etc/sudoers
+
+# Install basic tools
+sudo apt install -y build-essential git vim curl wget
+
+# Install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+source "$HOME/.asdf/asdf.sh"
+
+# Install asdf plugins
+asdf plugin add nodejs
+asdf install nodejs latest
+
+echo 'source "$HOME/.bash_user_profile"' >> ~/.bashrc
+
+# Link config files
+ln -sf "$HOME/dotfiles/.bash_user_profile" "$HOME/.bash_user_profile"
+ln -sf "$HOME/dotfiles/.asdfrc" "$HOME/.asdfrc"
+ln -sf "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+
+# Install utils
+sudo apt install -y gh wslu

7963a13e2df521115ce51757286f22a0d3a6d792
muro-yuta24-fixer <muro.yuta24@fixer.co.jp>
2024-05-09 10:07:02 +0900

    feat(nvim): add some plugins for Development

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +9,21 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+
+  -- Backend
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.docker" },
+
+  -- Frontend
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+
+  -- Config files
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.terraform" },
+
+  { import = "astrocommunity.pack.fish" },
 }

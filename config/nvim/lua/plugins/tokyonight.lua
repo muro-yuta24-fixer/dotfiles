bb18b3468de4bf4f8e9cd94b29626c1fbadf9ee9
muro-yuta24-fixer <muro.yuta24@fixer.co.jp>
2024-05-09 10:06:45 +0900

    feat(nvim): change colorscheme to tokyonight-storm

---
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "storm",
+    styles = {
+      comments = { italic = true },
+      types = { bold = true },
+      functions = { italic = true, bold = true },
+    },
+  },
+}

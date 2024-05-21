04d6feb698b8021b22e95ac7a8746e67195ab738
muro-yuta24-fixer <muro.yuta24@fixer.co.jp>
2024-05-21 13:38:25 +0900

    feat(nvim): add filetypes

---
+++ config/nvim/lua/utils/filetypes.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.web = {
+  "html",
+  "css",
+  "javascript",
+  "javascriptreact",
+  "javascript.jsx",
+  "typescript",
+  "typescriptreact",
+  "typescript.tsx",
+  "vue",
+  "svelte",
+  "astro",
+}
+
+return M

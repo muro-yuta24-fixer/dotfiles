f391390a7320c78ca0ea26ef56045a1803e86dc1
muro-yuta24-fixer <muro.yuta24@fixer.co.jp>
2024-05-22 17:15:09 +0900

    fix(nvim): load overseer.nvim with cmd

--- config/nvim/lua/plugins/runner.lua
+++ config/nvim/lua/plugins/runner.lua
@@ -1,6 +1,19 @@
 return {
   {
     "stevearc/overseer.nvim",
+    cmd = {
+      "OverseerOpen",
+      "OverseerToggle",
+      "OverseerSaveBundle",
+      "OverseerLoadBundle",
+      "OverseerRunCmd",
+      "OverseerRun",
+      "OverseerInfo",
+      "OverseerBuild",
+      "OverseerQuickAction",
+      "OverseerTaskAction",
+      "OverseerClearCache",
+    },
     keys = {
       { "<F5>", "<cmd>OverseerRun<cr>", desc = "Run a task" },
     },

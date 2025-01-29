f695f463a20d0fd9e550ae27763ced5d8511ba90
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-01-29 16:45:26 +0900

    🐛 fix neovim clipboard

--- config/config/nvim/lua/yuta/clipboard.lua
+++ config/config/nvim/lua/yuta/clipboard.lua
@@ -1,8 +1,8 @@
 vim.opt.clipboard = "unnamedplus"
 
-local win32yank_executable = "/mnt/c/Users/室雄太/AppData/Local/Microsoft/WinGet/Links/win32yank.exe"
-
 if vim.fn.has("wsl") then
+  local win32yank_executable = "/mnt/c/Users/室雄太/AppData/Local/Microsoft/WinGet/Links/win32yank.exe"
+
   vim.g.clipboard = {
     name = "win32yank-wsl",
     copy = {
@@ -10,9 +10,8 @@ if vim.fn.has("wsl") then
       ["*"] = win32yank_executable .. " -i --crlf",
     },
     paste = {
-
-      ["+"] = win32yank_executable .. " -o --crlf",
-      ["*"] = win32yank_executable .. " -o --crlf",
+      ["+"] = win32yank_executable .. " -o --lf",
+      ["*"] = win32yank_executable .. " -o --lf",
     },
     cache_enable = 0,
   }

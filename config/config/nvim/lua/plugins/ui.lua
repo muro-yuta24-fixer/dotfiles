020c4a1d5b10b55d3837f6b019af3ee1b3abee60
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-06-02 12:00:23 +0900

    💄(nvim,tmux) change separator

--- config/config/nvim/lua/plugins/ui.lua
+++ config/config/nvim/lua/plugins/ui.lua
@@ -78,8 +78,8 @@ return {
       options = {
         icons_enabled = true,
         theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         disabled_filetypes = {},
         ignore_focus = {},
         always_divide_middle = true,
@@ -246,9 +246,7 @@ return {
         local fg = palette.base
 
         return {
-          { "", guifg = bg },
           { " ", ft_icon, " ", filename, " ", guifg = fg, guibg = bg, gui = modified and "bold,italic" or "bold" },
-          { "", guifg = bg },
         }
       end,
     },

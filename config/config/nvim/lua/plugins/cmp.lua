7d84ebfe58e6b0994ec494d0ba4a56526143e429
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-06-09 16:37:01 +0900

    💄 (nvim) update appearance for completion

--- config/config/nvim/lua/plugins/cmp.lua
+++ config/config/nvim/lua/plugins/cmp.lua
@@ -5,9 +5,26 @@ return {
       local cmp = require("cmp")
 
       cmp.setup({
+        -- view = {
+        --   entries = "native",
+        -- },
         window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+          completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+          },
+        },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+            return kind
+          end,
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -45,6 +62,7 @@ return {
   },
   {
     "hrsh7th/cmp-cmdline",
+    enabled = false,
     event = "CmdlineEnter",
     dependencies = {
       "hrsh7th/nvim-cmp",
@@ -125,5 +143,8 @@ return {
         },
       })
     end,
+  },
+  {
+    "onsails/lspkind.nvim",
   },
 }

e7b60fb4bb5e2e089def5fe8419024dcdb71866f
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-07-08 16:33:07 +0900

    :sparkles: (nvim) 設定をシンプルにした

--- config/config/nvim/lua/conform-selector/builtins/init.lua
+++
@@ -1,5 +0,0 @@
-local M = {}
-
-M.javascript = require("conform-selector.builtins.javascript")
-
-return M
--- config/config/nvim/lua/snippets/csharp/init.lua
+++
@@ -1,3 +0,0 @@
-require("snippets.csharp.class-name")
-require("snippets.csharp.namespace")
-require("snippets.csharp.not-implemented-exception")

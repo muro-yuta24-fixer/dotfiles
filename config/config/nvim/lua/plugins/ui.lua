94af812c8d20a7bd165c2c50669070d7241f9553
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-07-23 20:04:11 +0900

    :lipstick: (tmux,nvim) セパレータを丸くした

--- config/config/nvim/lua/plugins/ui.lua
+++ config/config/nvim/lua/plugins/ui.lua
@@ -74,9 +74,7 @@ return {
       options = {
         icons_enabled = true,
         theme = "auto",
-        -- component_separators = { left = "", right = "" },
-        -- section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         disabled_filetypes = {},
         ignore_focus = {},

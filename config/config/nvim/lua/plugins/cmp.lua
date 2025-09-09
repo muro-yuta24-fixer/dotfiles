3f27e9c6cc829343f0a1ecee72eee566c8448073
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-09-09 20:08:48 +0900

    ✨ nvim-cmpからblink.cmpに乗り換えた

--- config/config/nvim/lua/plugins/cmp.lua
+++ config/config/nvim/lua/plugins/cmp.lua
@@ -1,151 +1,109 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      local lspkind = require("lspkind")
-
-      cmp.setup({
-        view = {
-          entries = "custom",
-        },
-        window = {
-          completion = {
-            -- winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-            col_offset = -3,
-            side_padding = 0,
-          },
-        },
-        formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-            local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = " " .. (strings[1] or "") .. " "
-            kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-            return kind
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-g>"] = cmp.mapping.abort(),
-          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        }),
-      })
-    end,
-  },
-  {
-    "hrsh7th/cmp-nvim-lsp",
-    event = "LspAttach",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-
-      cmp.setup({
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "nvim_lsp_signature_help" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
-  {
-    "hrsh7th/cmp-cmdline",
-    enabled = false,
-    event = "CmdlineEnter",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-    },
-    opts = function()
-      local cmp = require("cmp")
-
-      return {
-        {
-          type = ":",
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = cmp.config.sources({
-            { name = "path" },
-          }, {
-            { name = "cmdline" },
-          }),
-        },
-        {
-          type = "/",
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = cmp.config.sources({
-            { name = "buffer" },
-          }, {
-            -- { name = "nvim_lsp_document_symbol" },
-          }),
-        },
-        {
-          type = "?",
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = {
-            { name = "buffer" },
-          },
-        },
-      }
-    end,
-    config = function(_, opts)
-      local cmp = require("cmp")
-      vim.tbl_map(function(val) cmp.setup.cmdline(val.type, val) end, opts)
-    end,
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    version = "v2",
-    build = "make install_jsregexp",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-      require("snippets")
-
-      require("cmp").setup({
-        snippet = {
-          expand = function(args) require("luasnip").lsp_expand(args.body) end,
-        },
-      })
-    end,
-  },
-  {
-    "roobert/tailwindcss-colorizer-cmp.nvim",
+    "saghen/blink.cmp",
+    lazy = false,
+    version = "1.*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
-      color_square_width = 2,
-    },
-    config = function(_, opts)
-      local tailwind_colorizer_cmp = require("tailwindcss-colorizer-cmp")
-
-      tailwind_colorizer_cmp.setup(opts)
-
-      require("cmp").setup({
-        formatting = {
-          format = tailwind_colorizer_cmp.formatter,
-          fields = { "kind" },
-          expandable_indicator = true,
+      sources = {
+        default = {
+          "lsp",
+          "path",
+          "snippets",
+          "buffer",
         },
-      })
-    end,
+        per_filetype = {
+          markdown = { "path" },
+          lua = { "lazydev", "lsp", "path", "snippets", "buffer" },
+          gitcommit = { "gitmoji", "path" },
+        },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+          gitmoji = {
+            name = "gitmoji",
+            module = "gitmoji.blink",
+            opts = {
+              filetypes = { "gitcommit" },
+              completion = {
+                append_space = true,
+                complete_as = "emoji",
+              },
+            },
+          },
+        },
+      },
+      keymap = {
+        preset = "super-tab",
+      },
+      appearance = {
+        nerd_font_variant = "normal",
+      },
+      completion = {
+        menu = {
+          border = "rounded",
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      icon = dev_icon
+                    end
+                  else
+                    icon = require("lspkind").symbolic(ctx.kind, {
+                      mode = "symbol",
+                    })
+                  end
+
+                  return icon .. ctx.icon_gap
+                end,
+
+                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- You can also add the same function for `kind.highlight` if you want to
+                -- keep the highlight groups in sync with the icons.
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      hl = dev_hl
+                    end
+                  end
+                  return hl
+                end,
+              },
+            },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          window = {
+            border = "rounded",
+          },
+        },
+      },
+      signature = {
+        window = {
+          border = "rounded",
+        },
+      },
+      fuzzy = {
+        implementation = "rust",
+      },
+    },
   },
   {
     "onsails/lspkind.nvim",
+  },
+  {
+    "Dynge/gitmoji.nvim",
+    opts = {},
   },
 }

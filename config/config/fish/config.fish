b66e2341750b69c1696cef067c47b53a9c0f614e
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2026-02-27 15:52:07 +0900

    feat(fish): update config

--- config/config/fish/config.fish
+++ config/config/fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    mise activate fish | source
+    starship init fish | source
+    direnv hook fish | source
 
     # Alias settings
     if type eza >/dev/null 2>&1
@@ -68,16 +71,7 @@ if status is-interactive
     alias gl ghq-tmux
     alias pyvenv goto-venv
 
-    starship init fish | source
-    mise activate fish | source
-    direnv hook fish | source
+    fish_config theme choose 'Catppuccin Latte'
 
-    fish_config theme choose 'Catppuccin Mocha'
-
-    set -x FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
---color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
---color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
---color=selected-bg:#45475A \
---color=border:#6C7086,label:#CDD6F4"
+    set -x FZF_DEFAULT_OPTS "--color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 --color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 --color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 --color=selected-bg:#BCC0CC --color=border:#9CA0B0,label:#4C4F69"
 end

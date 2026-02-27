22568c118345e42ed6161a9c23813a1e934bdf09
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2026-02-27 15:51:50 +0900

    feat(wezterm): update config

--- config/config/wezterm/wezterm.lua
+++ config/config/wezterm/wezterm.lua
@@ -2,17 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.wsl_domains = {
-	{
-		name = "WSL:Arch",
-		distribution = "archlinux",
-		username = "yuta",
-		default_cwd = "~",
-		default_prog = { "fish", "--login", "--interactive" },
-	},
-}
-
-config.default_domain = "WSL:Arch"
+config.default_prog = { "/usr/bin/fish", "-l", "-i" }
 
 config.use_ime = true
 
@@ -24,11 +14,11 @@ config.font = wezterm.font_with_fallback({
 })
 config.font_size = 13.0
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Catppuccin Latte"
 
 config.window_decorations = "TITLE | RESIZE"
 
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_tab_bar = true
 config.tab_bar_at_bottom = false
@@ -36,53 +26,10 @@ config.tab_max_width = 64
 config.show_new_tab_button_in_tab_bar = false
 config.show_close_tab_button_in_tabs = false
 
-config.window_frame = {
-	font = wezterm.font({ family = "PlemolJP Console NF", weight = "Bold" }),
-	font_size = 13.0,
-	inactive_titlebar_bg = "#1e1e2e",
-	active_titlebar_bg = "#1e1e2e",
-}
-
-config.window_background_gradient = {
-	colors = { "#1e1e2e" },
-}
-
-config.colors = {
-	tab_bar = {
-		inactive_tab_edge = "none",
-	},
-}
-
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_left_half_circle_thick
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_right_half_circle_thick
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local background = "#cdd6f4"
-	local foreground = "#1e1e2e"
-	local edge_background = "none"
-	if tab.is_active then
-		background = "#89b4fa"
-		foreground = "#1e1e2e"
-	end
-	local edge_foreground = background
-	local title = " " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. " "
-	return {
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_LEFT_ARROW },
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_RIGHT_ARROW },
-	}
-end)
-
 -- すべてのデフォルトキーバインディングを無効化
 config.disable_default_key_bindings = true
 
-config.integrated_title_button_style = "Windows"
+-- config.integrated_title_button_style = "Gnome"
 
 config.keys = {
 	{

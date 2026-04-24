c6efc99f7550e0bd3475f341580100b18f9c234b
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2026-04-24 20:03:09 +0900

    feat(alacritty): Alacrittyの設定を変更

--- home/darwin.nix
+++ home/darwin.nix
@@ -4,9 +4,8 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      window.opacity = 0.95;
       font = {
-        size = 14;
+        size = 13;
         normal.family = "PlemolJP Console NF";
         bold.family = "PlemolJP Console NF";
         italic.family = "PlemolJP Console NF";

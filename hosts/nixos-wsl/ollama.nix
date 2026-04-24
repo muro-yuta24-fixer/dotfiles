0bfc2c59038a6697d93faeda6f23270f408ab14e
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2026-04-24 02:20:22 +0000

    feat(ollama): Ollamaの設定を少しやった

--- hosts/nixos-wsl/ollama.nix
+++ hosts/nixos-wsl/ollama.nix
@@ -12,7 +12,10 @@ in
     package = unstable.ollama-cpu;
     openFirewall = true;
     environmentVariables = {
-      OLLAMA_KEEP_ALIVE = "0";
+      OLLAMA_KEEP_ALIVE = "0"; # モデルをすぐにアンロードする
+      OLLAMA_CONTEXT_LENGTH = "65536"; # コンテキスト幅
+      OLLAMA_NUM_PARALLEL = "1"; # 同時に受けるリクエストの数
+      OLLAMA_NEW_ENGINE = "1";
     };
   };
 }

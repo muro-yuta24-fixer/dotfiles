7dac171a631cb7e49a6aada217524bd5b059560a
muro-yuta24-fixer <165998973+muro-yuta24-fixer@users.noreply.github.com>
2026-08-07 19:53:19 +0900

    feat(tmux): 画面分割のショートカット等をherdrっぽく変更

--- home/tmux.nix
+++ home/tmux.nix
@@ -20,15 +20,16 @@
       set -ag terminal-overrides ",xterm-256color:RGB"
 
       # Split window vertical
-      bind h split-window -h
+      bind v split-window -h
 
       # Split window horizontal
-      bind v split-window -v
+      bind - split-window -v
 
       set-option -g set-titles on
       set-option -g set-titles-string "#T"
 
       set -g pane-border-status top
+      set -g pane-border-format " #T "
 
       set -g mode-style "fg=blue,bg=#ccd0da"
 

cf2cf527f385750ba280cd1e6c137d020642ed3e
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2026-09-18 16:58:09 +0900

    ✨ (script) add install script for Ubuntu

---
+++ scripts/install-ubuntu.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+######################
+# Install Fish shell #
+######################
+sudo add-apt-repository ppa:fish-shell/release-4
+sudo apt update
+sudo apt install -y fish
+
+################
+# Install mise #
+################
+sudo apt install -y extrepo
+sudo extrepo enable mise
+sudo apt update
+sudo apt install -y mise
+
+##################
+# Install Docker #
+##################
+
+# Uninstall old versions
+sudo apt remove -y $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc docker-buildx podman-docker containerd runc | cut -f1)
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+# Install the Docker packages
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
